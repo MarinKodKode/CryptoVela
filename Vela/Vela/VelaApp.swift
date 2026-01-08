37a75df072dbc82706a8bd6f43e9c643d417a5c1
Manuel Alejandro Hernandez Marin <mhernandezm@inbursa.com>
2026-01-08 15:06:58 -0600

    [init project]

---
+++ Vela/Vela/VelaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VelaApp.swift
+//  Vela
+//
+//  Created by Manuel Alejandro Hernandez Marín on 08/01/26.
+//
+
+import SwiftUI
+
+@main
+struct VelaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Vela/VelaTests/VelaTests.swift
@@ -0,0 +1,17 @@
+//
+//  VelaTests.swift
+//  VelaTests
+//
+//  Created by Manuel Alejandro Hernandez Marín on 08/01/26.
+//
+
+import Testing
+@testable import Vela
+
+struct VelaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
